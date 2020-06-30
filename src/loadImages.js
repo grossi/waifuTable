89a856a171b7d825384cde4c1cadbdb821f6e9dd
rossig <rrossigabriel@gmail.com>
2020-06-30 19:16:23 -0300

    Adds deploy and listWaifu (for building the .json list) scripts.

--- src/loadImages.js
+++ src/loadImages.js
@@ -1,14 +1,4 @@
 const fs = require("fs");
-const util = require("util");
-const aws = require('aws-sdk');
-const env = require('./env.json');
-
-aws.config.update({
-    secretAccessKey: env.key,
-    accessKeyId: env.id,
-    region: env.region // region of your bucket
-});
-
 
 const normalizedPath = require("path").join(__dirname, "../public/waifus");
 
@@ -17,11 +7,11 @@ const waifus = fs.readdirSync(normalizedPath).map((file, k) => {
   const name = file.split('.')[0];
 
   return {
-    code: k,
+    code: `#${k}`,
     name,
     extension,
     location: `waifus/${file}`
   }
 }).filter(waifu => (waifu.extension === 'gif' || waifu.extension === 'mp4'));
 
-fs.writeFileSync('./waifus.json', JSON.stringify(waifus, null, 2), 'utf-8'); 
+fs.writeFileSync('./src/waifus.json', JSON.stringify(waifus, null, 2), 'utf-8'); 
